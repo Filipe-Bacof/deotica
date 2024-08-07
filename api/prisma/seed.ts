1b48e64e663101ce8f59a8eae1ed089b4a9bdfba
Filipe Bacof <filipebacof@gmail.com>
2024-08-07 05:03:09 -0300

    feat: review backend

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -7,7 +7,17 @@ const perfilUsuario = prisma.perfilUsuario.create({
   data: {
     id: 1,
     nome: "Administrador",
-    permissoes: ["ADMIN"],
+    permissoes: [
+      "ADMIN",
+      "CADASTRO_CLIENTE",
+      "CADASTRO_PRODUTO",
+      "EFETUAR_VENDA",
+      "GERAR_ORDEM_SERVICO",
+      "MANIPULAR_ESTOQUE",
+      "GERENCIAR_LANDING_PAGE",
+      "CRIAR_PERFIL_USUARIO",
+      "CRIAR_USUARIO",
+    ],
   },
 });
 
@@ -20,20 +30,12 @@ const usuario1 = prisma.usuarios.create({
   },
 });
 
-const usuario2 = prisma.usuarios.create({
-  data: {
-    email: "rafaelmahl@hotmail.com ",
-    nome: "Rafael Mahl",
-    senha: bcrypt.hashSync("Deotica$Admin2024", 10),
-    perfilId: 1,
-  },
-});
-
 async function run(): Promise<void> {
-  // await prisma.perfilUsuario.deleteMany();
-  // await prisma.usuarios.deleteMany();
+  await prisma.usuarios.deleteMany();
+  await prisma.perfilUsuario.deleteMany();
 
-  await Promise.all([perfilUsuario, usuario1, usuario2]);
+  await perfilUsuario;
+  await usuario1;
 }
 
 run()
